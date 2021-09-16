01d987dff25c29152de55bbce8f29ab3820cf7a8
Jason Pham <70985186+phamja1@users.noreply.github.com>
2021-09-16 17:26:15 -0400

    Check usernames and passwords on hardcoded values.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter your username:");
+        String user = s.nextLine();
+        System.out.println("Please enter your password");
+        String pass = s.nextLine();
+        if (isValidCred(user, pass)) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("That username or password does not exist.");
+        }
+    }
+
+    private static boolean isValidCred(String user, String pass) {
+        return user.equals("phamjason") && pass.equals("123");
+    }
 }
