41cf1c0a486f16b17d4a88ac088a1e04845bf176
Jason Pham <70985186+phamja1@users.noreply.github.com>
2021-10-24 22:07:16 -0400

    remove all

--- src/main/java/view/script.js
+++
@@ -1,3 +0,0 @@
-const name = document.getElementById('name')
-const name = document.getElementById('password')
-const name = document.getElementById('form')
--- src/main/resources/Status.java
+++
@@ -1,5 +0,0 @@
-public enum Status {
-    SUCCESS,
-    USER_ALREADY_EXISTS,
-    FAILURE
-}
